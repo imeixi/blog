8385ba5190e5ea221199b2b74ac451a98d11c182
zhengaihua <you@example.com>
2020-05-07 18:23:17 +0800

    Update operate_email.py

--- python_sync_article_from_email/operate_email.py
+++ python_sync_article_from_email/operate_email.py
@@ -17,47 +17,38 @@ class OperateEmail:
         self.imap_server = 'imap.163.com'
 
     def recv_email_by_pop3(self):
-        # 连接pop3服务器
+        print('Connecting...')
+        # 连接pop3服务器  # 身份认证
         server = poplib.POP3(self.pop3_server)
-
-        # 打开调试信息
-        server.set_debuglevel(2)
-
-        # 打印pop3服务器的欢迎文字
-        print(server.getwelcome())
-
-        # 身份认证
         server.user(self.username)
         server.pass_(self.password)
 
-        # stat return the numbers of the email and the size of space
-        print('server.stat()', server.stat())
+        # 打开调试信息
+        server.set_debuglevel(0)
 
-        # list()返回所有邮件的编号：
-        resp, mails, octets = server.list()
+        try:
+            # 打印pop3服务器的欢迎文字
+            print(server.getwelcome())
+            msg_count, msg_bytes = server.stat()
+            print('There are %s mail messages in %s' % (msg_count, msg_bytes))
 
-        # 查看所有邮件列表
-        print("all mails", mails)
+            print(server.list())
+            print('-' * 80)
+            input('[Press Enter to Continue]')
 
-        # 获取最新的一封邮件，注意索引从1开始
-        index = len(mails)
-        resp, lines, octets = server.retr(index)
+            for i in range(msg_count):
+                hdr, message, octets = server.retr(i+1)  # octets 是字节数
+                for line in message:
+                    print(line.decode('utf-8'))
+                print('-' * 80)
+                if i < msg_count-1:
+                    input('[Press Enter to Continue]')
 
-        print('resp', resp)
-        print('lines', lines)
-        print('octets', octets)
-
-        # lines 存储了邮件的原始文本的每一行
-        # 可以获得整个邮件的原始文本：
-        msg_content = b'\r\n'.join(lines).decode('utf-8')
-
-        # 稍后解析出邮件:
-        msg = Parser().parsestr(msg_content)
-
-        # 可以根据邮件索引号直接从服务器删除邮件:
-        # server.dele(index)
-        # 关闭连接:
-        server.quit()
+            # 可以根据邮件索引号直接从服务器删除邮件:
+            # server.dele(index)
+        finally:
+            # 关闭连接:
+            server.quit()
 
 
 if __name__ == '__main__':
