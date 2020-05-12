47386524ae4a5ce4dfbd39eff22aba1a5ba4103d
imeixi <z.ah_r@163.com>
2020-05-13 01:05:36 +0800

    finish receive email

--- python_sync_article_from_email/operate_email.py
+++ python_sync_article_from_email/operate_email.py
@@ -7,6 +7,9 @@ import smtplib
 import email.utils
 from email.message import Message
 from email.parser import Parser
+from email.header import decode_header
+from email.header import Header
+from email.utils import parseaddr
 
 
 class OperateEmail:
@@ -46,42 +49,6 @@ class OperateEmail:
         server.quit()
         print('bye\n' + '-' * 80)
 
-    def rec_email_by_pop3(self):
-        input('Ready fo receive emails?')
-        print('Connecting...')
-        # 连接pop3服务器  # 身份认证
-        server = poplib.POP3(self.pop3_server)
-        server.user(self.username)
-        server.pass_(self.password)
-
-        # 打开调试信息
-        server.set_debuglevel(0)
-
-        try:
-            # 打印pop3服务器的欢迎文字
-            print(server.getwelcome())
-            msg_count, msg_bytes = server.stat()
-            print('There are %s mail messages in %s' % (msg_count, msg_bytes))
-
-            print(server.list())
-            print('-' * 80)
-            input('[Press Enter to Continue]')
-
-            for i in range(msg_count):
-                hdr, message, octets = server.retr(i+1)  # octets 是字节数
-                for line in message:
-                    print(line.decode('utf-8'))
-                print('-' * 80)
-                if i < msg_count-1:
-                    input('[Press Enter to Continue]')
-
-            # 可以根据邮件索引号直接从服务器删除邮件:
-            server.dele(msg_count)
-
-        finally:
-            # 关闭连接:
-            server.quit()
-
     def send_mail_by_message(self):
         m = Message()
         m['from'] = self.email
@@ -100,6 +67,153 @@ class OperateEmail:
         print(x['From'])
         print(x.get_payload())
         print(x.items())
+
+    @staticmethod
+    def decode_str(s):
+        value, charset = decode_header(s)[0]
+        if charset:
+            value = value.decode(charset)
+        return value
+
+    @staticmethod
+    def guess_charset(msg):
+        # 先从msg对象获取编码:
+        charset = msg.get_charset()
+        if charset is None:
+            # 如果获取不到，再从Content-Type字段获取:
+            content_type = msg.get('Content-Type', '').lower()
+            pos = content_type.find('charset=')
+            if pos >= 0:
+                charset = content_type[pos + 8:].strip()
+        return charset
+
+    @staticmethod
+    def get_email_headers(msg):
+        # 邮件的From, To, Subject存在于根对象上:
+        headers = {}
+        for header in ['From', 'To', 'Subject', 'Date']:
+            value = msg.get(header, '')
+            if value:
+                if header == 'Date':
+                    headers['date'] = value
+                if header == 'Subject':
+                    # 需要解码Subject字符串:
+                    subject = OperateEmail.decode_str(value)
+                    headers['subject'] = subject
+                else:
+                    # 需要解码Email地址:
+                    hdr, address = parseaddr(value)
+                    print(hdr)
+                    print(address)
+                    name = OperateEmail.decode_str(hdr)
+                    value = u'%s <%s>' % (name, address)
+                    if header == 'From':
+                        from_address = value
+                        headers['from'] = from_address
+                    else:
+                        to_address = value
+                        headers['to'] = to_address
+        content_type = msg.get_content_type()
+        print('head content_type: ', content_type)
+        return headers
+
+    @staticmethod
+    def get_email_content(message, base_save_path):
+        j = 0
+        content = ''
+        attachment_files = []
+        for part in message.walk():
+            j = j + 1
+            file_name = part.get_filename()
+            content_type = part.get_content_type()
+            # 保存附件
+            if file_name:  # Attachment
+                # Decode filename
+                h = Header(file_name)
+                dh = decode_header(h)
+                print(dh)
+                filename = dh[0][0]
+                if dh[0][1]:  # 如果包含编码的格式，则按照该格式解码
+                    # filename = filename.decode(dh[0][1])
+                    filename = OperateEmail.decode_str(str(filename, dh[0][1])) #将附件名称可读化
+                    print(filename)
+                    print('_'*80)
+                    # filename = filename.encode("utf-8")
+                data = part.get_payload(decode=True)
+                att_file = open(str(base_save_path) + str(filename), 'wb')
+                attachment_files.append(filename)
+                att_file.write(data)
+                att_file.close()
+            elif content_type == 'text/plain' or content_type == 'text/html':
+                # 保存正文
+                data = part.get_payload(decode=True)
+                charset = OperateEmail.guess_charset(part)
+                if charset:
+                    charset = charset.strip().split(';')[0]
+                    print('charset:', charset)
+                    data = data.decode(charset)
+                content = data
+        return content, attachment_files
+
+    def rec_email_by_pop3(self):
+        print('Connecting...')
+        # 连接pop3服务器  # 身份认证
+        server = poplib.POP3(self.pop3_server)
+        server.user(self.username)
+        server.pass_(self.password)
+
+        # 打开调试信息
+        server.set_debuglevel(0)
+
+        try:
+            # 打印pop3服务器的欢迎文字
+            print(server.getwelcome())
+            msg_count, msg_bytes = server.stat()
+            print('There are %s mail messages in %s' % (msg_count, msg_bytes))
+
+            resp, mails, octets = server.list()
+            print('------ resp ------')
+            print(resp)  # +OK 46 964346 响应的状态 邮件数量 邮件占用的空间大小
+            print('------ mails ------')
+            print(mails)  # 所有邮件的编号及大小的编号list，['1 2211', '2 29908', ...]
+            print('------ octets ------')
+            print(octets)
+
+            # 获取最新一封邮件, 注意索引号从1开始:
+            msg_count = len(mails)
+            for i in range(msg_count):
+                resp, lines, octets = server.retr(i + 1)  # octets 是字节数
+                # lines存储了邮件的原始文本的每一行,
+                # 可以获得整个邮件的原始文本:
+                msg_content = b'\r\n'.join(lines).decode('utf-8')
+
+                # 把邮件内容解析为Message对象：
+                msg = Parser().parsestr(msg_content)
+
+                print(msg.get('From'))
+                print(msg.get('To'))
+                print(msg.get('Subject'))
+
+                # 但是这个Message对象本身可能是一个MIMEMultipart对象，即包含嵌套的其他MIMEBase对象，
+                # 嵌套可能还不止一层。所以我们要递归地打印出Message对象的层次结构：
+                print('---------- 解析之后 ----------')
+                base_save_path = './email_attachments/'
+                msg_headers = OperateEmail.get_email_headers(msg)
+                content, attachment_files = OperateEmail.get_email_content(msg, base_save_path)
+
+                print('subject:', msg_headers['subject'])
+                print('from_address:', msg_headers['from'])
+                print('to_address:', msg_headers['to'])
+                print('date:', msg_headers['date'])
+                print('content:', content)
+                # print('attachment_files: ', attachment_files)
+
+            # 可以根据邮件索引号直接从服务器删除邮件:
+            # server.dele(msg_count)
+
+        finally:
+            # 关闭连接:
+            server.quit()
 
 
 if __name__ == '__main__':
